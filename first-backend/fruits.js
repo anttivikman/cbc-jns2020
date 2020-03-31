898c02aec703525f21cf7fe477443b89cd7e42e3
Antti Vikman <antti.vikman@nolwenture.com>
2020-03-31 08:28:35 +0300

    Make DB configurable

--- first-backend/fruits.js
+++ first-backend/fruits.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-    host: 'localhost',
-    port: 5432,
-    database: 'sample',
-    user: 'postgres',
-    password: 'postgres'
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || 5432,
+    database: process.env.POSTGRES_DB || 'sample',
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres'
 });
 
 const getFruits = (req, res) => 
